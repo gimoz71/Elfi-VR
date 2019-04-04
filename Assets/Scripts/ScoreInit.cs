f1fbb5aeb3466f5cd440adc2151c93fb3a43655f
Gianluca Monti <gm3871@gmail.com>
2019-04-04 17:35:56 +0200

    wip score table

---
+++ Assets/Scripts/ScoreInit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScoreInit : MonoBehaviour
+{
+    public static int scoreInc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Give the PlayerPrefs some values to send over to the next Scene
+        PlayerPrefs.SetInt("Score", 0);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
