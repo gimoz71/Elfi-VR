7f2938082c4a6946579c2d12b6cd95936de7374c
Gianluca Monti <gm3871@gmail.com>
2019-04-04 10:04:00 +0200

    init repo

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour {
+
+    public string sceneSelect;
+
+     public void sceneChange()
+    {
+        SceneManager.LoadScene(sceneSelect);
+    }
+}
