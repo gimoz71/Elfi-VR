e7ec3be96600f0a11897f4668124b18daf50fade
Gianluca Monti <gm3871@gmail.com>
2019-04-15 19:36:18 +0200

    wip ghost

---
+++ Assets/Scripts/PartCollision.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[ExecuteInEditMode]
+public class PartCollision : MonoBehaviour
+{
+    public ParticleSystem part;
+    public List<ParticleCollisionEvent> collisionEvents;
+
+    public GameObject sword;
+    public GameObject player;
+
+    void Start()
+    {
+        part = GetComponent<ParticleSystem>();
+        collisionEvents = new List<ParticleCollisionEvent>();
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        int numCollisionEvents = part.GetCollisionEvents(other, collisionEvents);
+
+        //Rigidbody rb = other.GetComponent<Rigidbody>();
+        int i = 0;
+
+        while (i < numCollisionEvents)
+        {
+            Debug.Log(i);
+            if (other.name == "HeadCollider")
+            {
+                
+                Debug.Log("COLLISION!!!");
+            } else if (other.name == "sword")
+            {
+                Debug.Log("KILL PARTICLE!!!!!");
+            }
+            i++;
+        }
+    }
+}
---
+++ Assets/Scripts/ParticleControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ParticleControl : MonoBehaviour
+{
+
+
+
+
+    public ParticleSystem pSystem;
+    public Text controlParticle;
+
+    public void Start()
+    {
+        pSystem.Stop();
+        //pSystem.Stop();
+    }
+
+    public void ParticleToggle()
+    {
+        if(pSystem.isPaused == true || pSystem.isStopped == true)
+        {
+            pSystem.Play();
+            controlParticle.text = "Pause";
+        } else
+        {
+            //pSystem.Pause();
+            pSystem.Stop();
+            pSystem.Clear();
+            controlParticle.text = "Play";
+        }
+    }
+}
